ae500aef206cc085524bfde7cc695653d63002fa
Michael Davis <jrmdd1496@gmail.com>
2023-04-04 17:15:40 -0500

    Removed one more unnecessary code block.

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,12 +11,3 @@
         address: Faker::Address.street_address
     )
 end
-
-10.times do
-    User.create(
-        first_name: Faker::Name.first_name,
-        last_name: Faker::Name.last_name,
-        email: Faker::Internet.email,
-        password_digest: Faker::Internet.password
-    )   
-end
